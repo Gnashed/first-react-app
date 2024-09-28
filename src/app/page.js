595cbc1f2ecf60896b902362fc330f04768c6a6e
Tion Blackmon <tion.blackmon@outlook.com>
2024-09-28 10:58:02 -0500

    Part one complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,55 @@
 'use client';
 
- // any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
-
-import { Button } from 'react-bootstrap';
-import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
+// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
+// anything in the src dir, you can use the @ instead of relative paths
 import { useAuth } from '@/utils/context/authContext';
+import { useEffect, useState } from 'react';
 
 function Home() {
+  // Since the returned data is an object, we must set the initial state as an empty object.
+  const [uselessFact, SetUselessFact] = useState({});
   const { user } = useAuth();
 
+  const fetchFact = async () => {
+    const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
+    const fact = await response.json();
+
+    SetUselessFact(fact);
+  };
+
+  const selectResponse = (boolean) => {
+    const obj = {
+      userId: user.uid,
+      permaLink: uselessFact.permalink,
+      response: boolean,
+    };
+    fetchFact();
+    return obj;
+  };
+
+  /*
+  Without a useEffect hook, calling fetchFact() would continuously loop through fetches.
+
+  useEffect runs after the rendering/re-rendering of the component only if its dependencies change.
+    -> Runs after the component is rendered (mounted).
+    -> Accepts two arguments - the function you want to execute when useEffect runs and a dependency array. The dependency array keeps track of value updates that will trigger the useEffect to run again and rerender the component.
+  */
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      <h1>Fact</h1>
+      <h5>{uselessFact.text}</h5>
+      <h6>Did you know this fact? 🤔</h6>
+      <button className="btn btn-success" type="button" onClick={() => selectResponse(true)}>
+        YES
+      </button>
+      <button className="btn btn-danger" type="button" onClick={() => selectResponse(false)}>
+        NO
+      </button>
+    </>
   );
 }
 
